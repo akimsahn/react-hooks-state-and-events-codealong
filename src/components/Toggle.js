e178807e76ef73d5403106e7ae3f57acdde5fec2
Allison Kim <akeem159@yahoo.com>
2022-08-10 11:43:01 -0400

    commit tree 8fc90288fc9f69e519a67d3a592ac13bc5a9b35d

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [ onOff, setOnOff ] = useState(false)
+
+  function toggleButton() {
+    setOnOff((onOff) => !onOff)
+  }
+
+  const color = onOff ? 'red' : 'white'
+
+  return <button style={{background: color}} onClick={toggleButton}>{onOff ? 'On' : 'Off'}</button>;
 }
 
 export default Toggle;
